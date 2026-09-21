96edd1ab2715fa4b806507fcd8116146dfd0640e
lisyarus <lisyarus@gmail.com>
2026-09-22 01:38:52 +0300

    Add vector4ub alias

--- cpp/math/include/math/aliases.hpp
+++ cpp/math/include/math/aliases.hpp
@@ -5,11 +5,15 @@
 #include <math/quaternion.hpp>
 #include <math/vector.hpp>
 
+#include <cstdint>
+
 namespace math {
 
 using vector2f = vector<float, 2>;
 using vector3f = vector<float, 3>;
 using vector4f = vector<float, 4>;
+
+using vector4ub = vector<std::uint8_t, 4>;
 
 using matrix2f = matrix<float, 2, 2>;
 using matrix3f = matrix<float, 3, 3>;
